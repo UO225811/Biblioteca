88e8c87a6877a584ff102f6b9c4c9d63d490e7f7
UO225811 <UO225811@uniovi.es>
2023-10-24 17:24:22 +0200

    Added model files

---
+++ src/main/java/com/capgemini/model/Copia.java
@@ -0,0 +1,9 @@
+package com.capgemini.model;
+
+enum EstadoCopia {
+	PRESTADO, RETRASO, BIBLIOTECA, REPARACION
+}
+
+public class Copia {
+
+}
---
+++ src/main/java/com/capgemini/model/Libro.java
@@ -0,0 +1,9 @@
+package com.capgemini.model;
+
+enum TipoLibro {
+	NOVELA, TEATRO, POESIA, ENSAYO
+}
+
+public class Libro {
+
+}
