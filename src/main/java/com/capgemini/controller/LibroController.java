d429ddbd9430cc787854f5e9b7f57c5461f37794
UO225811 <UO225811@uniovi.es>
2023-10-25 11:38:42 +0200

    Updated controller and impl from book

--- src/main/java/com/capgemini/controller/LibroController.java
+++ src/main/java/com/capgemini/controller/LibroController.java
@@ -1,5 +1,73 @@
 package com.capgemini.controller;
 
-public class LibroController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.capgemini.model.Libro;
+import com.capgemini.service.LibroService;
+
+@Controller
+public class LibroController {
+	
+	@Autowired
+	private LibroService libroService;
+
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		return findPaginated(1, "nombreLibro", "asc", model);
+	}
+
+	@PostMapping("/save")
+	public String saveCourse(@ModelAttribute("course") Libro libro) {
+		libroService.saveBook(libro);
+		return "redirect:/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteCourse(@PathVariable(value="id") long id) {
+		this.libroService.deleteBookById(id);
+		return "redirect:/";
+	}
+
+	@GetMapping("/update/{id}")
+	public String showFormForUpdating(@PathVariable(value="id") long id, Model model) {
+		Libro libro = this.libroService.getBookById(id);
+		model.addAttribute("course", libro);
+		return "actualizar_Libro";
+	}
+
+	@GetMapping("/add")
+	public String showNewCourseForm(Model model) {
+		Libro libro = new Libro();
+		model.addAttribute("course", libro);
+		return "nuevo_Libro";
+	}
+
+	@GetMapping("/page/{pageNumber}")
+	public String findPaginated(@PathVariable(value = "pageNumber") int pageNum,
+			@RequestParam("sortField") String sortField, @RequestParam("sortOrder") String sortOrder, Model model) {
+		int pageSize = 4;
+		Page<Libro> page = libroService.findPaginated(pageNum, pageSize, sortField, sortOrder);
+		List<Libro> books = page.getContent();
+
+		model.addAttribute("sortOrder", sortOrder);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("books", books);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("reverseSortDir", sortOrder.equalsIgnoreCase("asc") ? "desc" : "asc");
+
+		return "index";
+	}
+	
 }
--- src/main/java/com/capgemini/service/impl/LibroServiceImpl.java
+++ src/main/java/com/capgemini/service/impl/LibroServiceImpl.java
@@ -1,42 +1,59 @@
 package com.capgemini.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import com.capgemini.model.Libro;
+import com.capgemini.repository.LibroRepository;
 import com.capgemini.service.LibroService;
 
+@Service
 public class LibroServiceImpl implements LibroService {
 
+	@Autowired
+	private LibroRepository libroRepository;
+	
 	@Override
 	public Libro getBookById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Libro> opt = libroRepository.findById(id);
+		Libro libro = null;
+		if (opt.isPresent())
+			libro = opt.get();
+		else
+			throw new RuntimeException("Book with id " + id + " not found");
+		return libro;
 	}
 
 	@Override
 	public List<Libro> getAllBooks() {
-		// TODO Auto-generated method stub
-		return null;
+		return libroRepository.findAll();
 	}
 
 	@Override
 	public void saveBook(Libro l) {
-		// TODO Auto-generated method stub
-
+		libroRepository.save(l);
 	}
 
 	@Override
 	public void deleteBookById(long id) {
-		// TODO Auto-generated method stub
-
+		libroRepository.deleteById(id);
 	}
 
 	@Override
 	public Page<Libro> findPaginated(int pageNum, int pageSize, String sortField, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
+		Sort sort = sortOrder.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending()
+				: Sort.by(sortField).descending();
+
+		Pageable pageable = PageRequest.of(pageNum-1, pageSize, sort);
+
+		return this.libroRepository.findAll(pageable);
 	}
 
 }
