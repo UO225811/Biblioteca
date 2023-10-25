119647776247a74df0e3c845cf50fb3b72ca622a
Alberto Rivas <UO252177@uniovi.es>
2023-10-25 13:14:05 +0200

    Primera versión del modelo

--- src/main/java/com/capgemini/model/Autor.java
+++ src/main/java/com/capgemini/model/Autor.java
@@ -1,12 +1,15 @@
 package com.capgemini.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,4 +26,7 @@ public class Autor {
 	private String nacionalidad;
 	@Column
 	private LocalDate fechaNacimiento;
+	
+	@OneToMany(mappedBy="autor", targetEntity=Libro.class, cascade=CascadeType.ALL)
+	private Set<Libro> obras;
 }
--- src/main/java/com/capgemini/model/Libro.java
+++ src/main/java/com/capgemini/model/Libro.java
@@ -1,10 +1,17 @@
 package com.capgemini.model;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 enum TipoLibro {
@@ -27,4 +34,11 @@ public class Libro {
 	private String editorial;
 	@Column
 	private int anyo;
+	
+	@OneToMany(mappedBy="libro", targetEntity=Copia.class, cascade=CascadeType.ALL)
+	private Set<Copia> copias;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="fk_autor")
+	private Autor autor;
 }
