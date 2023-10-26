44e7ca158b613cac6af959bec9b9d3cb0f902768
UO225811 <UO225811@uniovi.es>
2023-10-26 12:19:48 +0200

    Añadido CopiaRepository

--- src/main/java/com/capgemini/repository/CopiaRepository.java
+++ src/main/java/com/capgemini/repository/CopiaRepository.java
@@ -1,9 +1,17 @@
 package com.capgemini.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.capgemini.model.Copia;
 
-public interface CopiaRepository extends JpaRepository<Copia, Long>{
+public interface CopiaRepository extends JpaRepository<Copia, Long> {
+	
+	@Query("SELECT count(*) from copias where fk_libro= :libroid")
+	int getNumCopiasByLibroId(@Param("libroid") long libroId);
 
+	@Query("INSERT INTO copias(estado,fk_libro) values(3,:libroid)")
+	int createCopiasByLibroId(@Param("libroid") long libroId, int numCopias);
+	
 }
