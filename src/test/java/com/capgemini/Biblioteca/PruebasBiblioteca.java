ac3a625785bee68da278af77f7d512fc1f06525e
Alberto Rivas <UO252177@uniovi.es>
2023-11-03 12:47:51 +0100

    Actualizado pruebas

--- src/test/java/com/capgemini/Biblioteca/PruebasBiblioteca.java
+++ src/test/java/com/capgemini/Biblioteca/PruebasBiblioteca.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,17 +18,17 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PruebasBiblioteca {
-	private WebDriver driver;
+	private static WebDriver driver;
 	
-	@BeforeEach
+	@BeforeAll
 	public void setUp() {
 		System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
 		driver = new EdgeDriver(new EdgeOptions());
 		driver.manage().window().maximize();
 		driver.get("http://localhost:8080");
 	}
-
 	
 	@Test
 	void testLogIn() {
@@ -36,15 +39,20 @@ public class PruebasBiblioteca {
 		WebElement password = this.driver.findElement(By.name("password"));
 		password.clear();
 		password.sendKeys("1234");
-		
-		
-		
+	
 		password.submit();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		assertEquals("Libros", driver.getTitle());
+		assertEquals("http://localhost:8080/home", driver.getCurrentUrl());
 	}
 	
-	@AfterEach
+	@Test
+	void testLogOut() {
+		WebElement logout = this.driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/li[2]/form/input[1]"));
+		logout.click();
+		assertEquals("http://localhost:8080/login", driver.getCurrentUrl());
+	}
+	
+	@AfterAll
 	public void tearDown() {
         if (driver != null) {
             driver.quit();
