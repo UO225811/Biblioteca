40c534a4ea8c81eaa1b198dc9de2cd6ab9ef277e
Alberto Rivas <UO252177@uniovi.es>
2023-11-03 12:12:53 +0100

    Clases de pruebas Selenium

--- src/test/java/com/capgemini/Biblioteca/PruebasBiblioteca.java
+++ src/test/java/com/capgemini/Biblioteca/PruebasBiblioteca.java
@@ -4,32 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class PruebasBiblioteca {
 	private WebDriver driver;
 	
 	@BeforeEach
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "./src.test.resources.chromedriver.chromedriver.exe");
-		driver = new ChromeDriver(new ChromeOptions());
+		System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
+		driver = new EdgeDriver(new EdgeOptions());
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com");
+		driver.get("http://localhost:8080");
 	}
 
+	
 	@Test
 	void testLogIn() {
-		WebElement searchBox = this.driver.findElement(By.name("q"));
-		searchBox.clear();
-		searchBox.sendKeys("covid argentina");
-		searchBox.submit();
+		WebElement username = this.driver.findElement(By.name("username"));
+		username.clear();
+		username.sendKeys("grogu@email.com");
+		
+		WebElement password = this.driver.findElement(By.name("password"));
+		password.clear();
+		password.sendKeys("1234");
+		
+		
+		
+		password.submit();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		assertEquals("Dosis de refuerzo vacuna COVID-19", driver.getTitle());
+		assertEquals("Libros", driver.getTitle());
+	}
+	
+	@AfterEach
+	public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
 	}
 }
